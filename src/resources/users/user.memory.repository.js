3f22f5adc02e242b5eb49f573f1becd6c1430090
Kroh Nickolay <375336623761@ya.ru>
2020-10-12 22:18:52 +0300

    feat: add user's data

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,13 @@
+const User = require('./user.model');
+
 const getAll = async () => {
   // TODO: mock implementation. should be replaced during task development
-  return [];
+  const users = [];
+  users.push(new User({ name: 'John', login: 'john', password: '123qwe' }));
+  users.push(new User({ name: 'Ann', login: 'anna', password: 'secret' }));
+  users.push(new User({ name: 'Isaac', login: 'newton', password: 'apple' }));
+
+  return users;
 };
 
 module.exports = { getAll };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,20 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
+
+  // res.statusCode = 202;
+  // res.setHeader('Content-Type', 'application/json');
   // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  const users = await usersService.getAll();
+
+  // res.statusCode = 202;
+  // res.setHeader('Content-Type', 'application/json');
+  // map user fields to exclude secret fields like "password"
+  res.json(User.toResponse(users[1]));
 });
 
 module.exports = router;
